3256e5719ad2dbc0397b42a690d11451157661f9
pcraochallas@calibrage.in <pcraochallas@calibrage.in>
2022-04-27 13:54:27 +0530

    Eye icon is added in login form

--- src/app/account/login.module.ts
+++ src/app/account/login.module.ts
@@ -14,25 +14,6 @@ import { PatientLoginComponent } from './Patientlogin.component';
 import { PartnerSignupComponent } from '../patient/partner-signup.component';
 import { CreatePasswordComponent } from './createpassword.component';
 
-/*
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { ToastrModule } from 'ngx-toastr';
-import { SharedModule } from './_common/shared';
-import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
-import { MatSelectFilterModule } from 'mat-select-filter';
-import { MatTableExporterModule } from 'mat-table-exporter';
-import { Ng2OrderModule } from 'ng2-order-pipe';
-import { NgxPaginationModule } from 'ngx-pagination';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-*/
-
 @NgModule({
   imports: [
     CommonModule,
