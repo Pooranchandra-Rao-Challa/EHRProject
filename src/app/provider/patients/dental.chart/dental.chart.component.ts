036af777d4a71d1ec3948ccfd09e48c59c65dd8e
RiyaGupta10 <riya@cis10137>
2022-04-29 00:35:06 -0700

    Interagte Checkbox tree functionality in Desntal-Chart Screen

--- src/app/provider/patients/dental.chart/dental.chart.component.ts
+++ src/app/provider/patients/dental.chart/dental.chart.component.ts
@@ -9,11 +9,52 @@ export class DentalChartComponent implements OnInit {
 
   AdultPrem:boolean=true;
   ChilPrim:boolean=false;
+  checboxList:any=[];
   constructor() { }
 
   ngOnInit( ): void {
+    this.GetList();
+    var checks = document.querySelectorAll("input[type=checkbox]");
+    for(var i = 0; i < checks.length; i++){
+      checks[i].addEventListener( 'change', function() {
+        if(this.checked) {
+           showChildrenChecks(this);
+        } else {
+           hideChildrenChecks(this)
+        }
+      });
+    }
+    function showChildrenChecks(elm) {
+       var pN = elm.parentNode;
+       var childCheks = pN.children;
+      for(var i = 0; i < childCheks.length; i++){
+          if(hasClass(childCheks[i], 'child-check')){
+              childCheks[i].classList.add("active");
+          }
+      }
+    }
+    function hideChildrenChecks(elm) {
+       var pN = elm.parentNode;
+       var childCheks = pN.children;
+      for(var i = 0; i < childCheks.length; i++){
+          if(hasClass(childCheks[i], 'child-check')){
+              childCheks[i].classList.remove("active");
+          }
+      }
+    }
+    function hasClass(elem, className) {
+        return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+    }
   }
 
+  GetList()
+  {
+    this.checboxList=[
+      {listId:"1",listName:'D0120 periodic oral evaluation - established patient'},
+      {listId:"2",listName:'D0120 periodic oral evaluation - established patient'},
+      {listId:"3",listName:'D0120 periodic oral evaluation - established patient'}
+    ]
+  }
   AdultPerm()
   {
     this.AdultPrem=true;
@@ -24,5 +65,7 @@ export class DentalChartComponent implements OnInit {
    this.AdultPrem=false;
    this.ChilPrim=true;
   }
+  OpenCheck(){
 
+  }
 }
