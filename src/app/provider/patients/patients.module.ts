54cb559328a2665de07fbb562c0d2449ea8f58b1
Saikarthik Thotakura <saikarthikthotakura@gmail.com>
2022-05-16 13:03:51 +0530

    Ui changes for smart schedule, setting & patients screens and implemented search filter for patients screen.

--- src/app/provider/patients/patients.module.ts
+++ src/app/provider/patients/patients.module.ts
@@ -11,10 +11,11 @@ import { InsuranceComponent } from './insurance/insurance.component';
 import { AmendmentsComponent } from './amendments/amendments.component';
 import { ResetPasswordComponent } from './resetpassword/resetpassword.component';
 import { CqmsNotPerformedComponent } from './cqms.not.performed/cqms.not.performed.component';
+import { SearchPipe } from './search.pipe';
 @NgModule({
   imports: [SharedModule, CommonModule],
   exports: [PatientsComponent, PatientDetailsComponent, ChartComponent, DentalChartComponent, ProfileComponent, InsuranceComponent, AmendmentsComponent, ResetPasswordComponent, CqmsNotPerformedComponent],
-  declarations: [PatientsComponent, PatientDetailsComponent, ChartComponent, DentalChartComponent, ProfileComponent, InsuranceComponent, AmendmentsComponent, ResetPasswordComponent, CqmsNotPerformedComponent],
+  declarations: [PatientsComponent, PatientDetailsComponent, ChartComponent, DentalChartComponent, ProfileComponent, InsuranceComponent, AmendmentsComponent, ResetPasswordComponent, CqmsNotPerformedComponent, SearchPipe],
   providers: [],
   // schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
---
+++ src/app/provider/patients/search.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+
+  transform(value: any, keys: any, term: string): string {
+    debugger;
+    if (!term) {
+      return value;
+    }
+    return (value || []).filter((item) =>
+      keys
+        .split(',')
+        .find(
+          (key) =>
+            item.hasOwnProperty(key) && new RegExp(term, 'gi').test(item[key])
+        )
+    );
+  }
+
+}
