8a62834e8968b3e4dc10e71e32d598bd5f7d6e48
shweta-ops <shweta@calibrage.in>
2022-05-18 10:59:14 +0530

    integrated Message  screen in provider module

--- src/app/provider/messages/messages.component.ts
+++ src/app/provider/messages/messages.component.ts
@@ -1,11 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { OverlayService } from '../../overlay.service';
+import {  TemplateRef } from '@angular/core';
+import { ComponentType } from '@angular/cdk/portal';
+import { NewmessageDialogComponent } from '../../dialogs/newmessage.dialog/newmessage.dialog.component';
 @Component({
   selector: 'app-labs.imaging',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.scss']
 })
-export class MessagesComponent implements OnInit {
-  ngOnInit(): void {
+export class MessagesComponent {
 
+  displayReq = "none";
+  MessageDialogComponent = NewmessageDialogComponent;
+  DialogResponse = null;
+
+
+  constructor(private overlayService :OverlayService) { }
+
+  ngOnInit(): void {
+  }
+  openComponentDialogmessage(content: TemplateRef<any> | ComponentType<any> | string) {
+    const ref = this.overlayService.open(content, null);
+
+    ref.afterClosed$.subscribe(res => {
+      if (typeof content === 'string') {
+      //} else if (content === this.yesNoComponent) {
+        //this.yesNoComponentResponse = res.data;
+      }
+      else if (content === this.MessageDialogComponent) {
+        this.DialogResponse = res.data;
+      }
+    });
   }
 }
