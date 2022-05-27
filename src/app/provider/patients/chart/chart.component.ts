8ddd9f5b57948251f1099ecc248fbcb0f56a90e6
saikarthik1006 <saikarthik@calibrage.in>
2022-05-27 13:15:43 +0530

    worked on data binding in chart screen

--- src/app/provider/patients/chart/chart.component.ts
+++ src/app/provider/patients/chart/chart.component.ts
@@ -61,9 +61,9 @@ export class ChartComponent implements OnInit {
 
   // Common Reqparams for chart screen
   reqparams = {
-    ClinicId: '5836d8f2f2e48f33037e5e39',
-    ProviderId: '5836d7d1f2e48f310385069c',
-    PatientId: '5836dafef2e48f36ba90a990'
+    // ClinicId: '5836d8f2f2e48f33037e5e39',
+    // ProviderId: '5836d7d1f2e48f310385069c',
+    PatientId: '5836dafef2e48f36ba90a996'
   }
 
   // Get advanced directives info
@@ -75,84 +75,85 @@ export class ChartComponent implements OnInit {
 
   // Get diagnoses info
   DiagnosesByPatientId() {
-    // let reqparams = {
-    //   ClinicId: '59307584bc61173526a63361',
-    //   ProviderId: '59307582bc61173526a63336',
-    //   PatientId: '59841816bc61176759627700'
-    // }
-    this.patientService.DiagnosesByPatientId(this.reqparams).subscribe((resp) => {
+    let reqparams = {
+      // ClinicId: '59307584bc61173526a63361',
+      // ProviderId: '59307582bc61173526a63336',
+      PatientId: '59841816bc61176759627700'
+    }
+    this.patientService.DiagnosesByPatientId(reqparams).subscribe((resp) => {
       this.allDiagnoses = resp.ListResult;
     });
   }
 
   // Get allergies info
   AllergiesByPatientId() {
-    // let reqparams = {
-    //   ClinicId: '5953c325bc611739aaf8c0e7',
-    //   ProviderId: '5953c323bc611739aaf8c0bc',
-    //   PatientId: '59dd3796bc6117730c9ca3d3'
-    // }
-    this.patientService.AllergiesByPatientId(this.reqparams).subscribe((resp) => {
+    let reqparams = {
+      // ClinicId: '5953c325bc611739aaf8c0e7',
+      // ProviderId: '5953c323bc611739aaf8c0bc',
+      PatientId: '59dd3796bc6117730c9ca3d3'
+    }
+    this.patientService.AllergiesByPatientId(reqparams).subscribe((resp) => {
       this.allAllergies = resp.ListResult;
     });
   }
 
   // Get Past Medical Histories info
   PastMedicalHistoriesByPatientId() {
-    // let reqparams = {
-    //   ClinicId: '5953c325bc611739aaf8c0e7',
-    //   ProviderId: '5953c323bc611739aaf8c0bc',
-    //   PatientId: '59dd3796bc6117730c9ca3d3'
-    // }
-    this.patientService.PastMedicalHistoriesByPatientId(this.reqparams).subscribe((resp) => {
+    let reqparams = {
+      // ClinicId: '5953c325bc611739aaf8c0e7',
+      // ProviderId: '5953c323bc611739aaf8c0bc',
+      PatientId: '59dd3796bc6117730c9ca3d3'
+    }
+    this.patientService.PastMedicalHistoriesByPatientId(reqparams).subscribe((resp) => {
       this.pastMedicalHistories = resp.ListResult;
+      console.log(this.pastMedicalHistories);
     });
   }
 
   // Get Immunizations info
   ImmunizationsByPatientId() {
-    // let reqparams = {
-    //   ClinicId: '59307584bc61173526a63361',
-    //   ProviderId: '59307582bc61173526a63336',
-    //   PatientId: '5ab94defbc61173a61c5cf1c'
-    // }
-    this.patientService.ImmunizationsByPatientId(this.reqparams).subscribe((resp) => {
+    let reqparams = {
+      // ClinicId: '59307584bc61173526a63361',
+      // ProviderId: '59307582bc61173526a63336',
+      PatientId: '5ab94defbc61173a61c5cf1c'
+    }
+    this.patientService.ImmunizationsByPatientId(reqparams).subscribe((resp) => {
       this.immunizations = resp.ListResult;
     });
   }
 
   // Get medications info
   MedicationsByPatientId() {
-    // let reqparams = {
-    //   ClinicId: '5953c325bc611739aaf8c0e7',
-    //   ProviderId: '5953c323bc611739aaf8c0bc',
-    //   PatientId: '59e8c25bbc61170be57c869f'
-    // }
-    this.patientService.MedicationsByPatientId(this.reqparams).subscribe((resp) => {
+    let reqparams = {
+      // ClinicId: '5953c325bc611739aaf8c0e7',
+      // ProviderId: '5953c323bc611739aaf8c0bc',
+      PatientId: '59e8c25bbc61170be57c869f'
+    }
+    this.patientService.MedicationsByPatientId(reqparams).subscribe((resp) => {
       this.medications = resp.ListResult;
     });
   }
 
   // Get encounters info
   EncountersByPatientId() {
-    // let reqparams = {
-    //   ClinicId: '5836d8f2f2e48f33037e5e39',
-    //   ProviderId: '5836d7d1f2e48f310385069c',
-    //   PatientId: '5836dafef2e48f36ba90a996'
-    // }
-    this.patientService.EncountersByPatientId(this.reqparams).subscribe((resp) => {
+    let reqparams = {
+      // ClinicId: '5836d8f2f2e48f33037e5e39',
+      // ProviderId: '5836d7d1f2e48f310385069c',
+      PatientId: '5836dafef2e48f36ba90a996'
+    }
+    this.patientService.EncountersByPatientId(reqparams).subscribe((resp) => {
       this.encounters = resp.ListResult;
     });
   }
 
   // Get appointments info
   AppointmentsByPatientId() {
-    // let reqparams = {
-    //   ClinicId: '5836d8eff2e48f33037e5cd5',
-    //   ProviderId: '5836d7cff2e48f3103850699',
-    //   PatientId: '5836db02f2e48f36ba90ac4a'
-    // }
-    this.patientService.AppointmentsByPatientId(this.reqparams).subscribe((resp) => {
+    let reqparams = {
+      // ClinicId: '5836d8eff2e48f33037e5cd5',
+      // ProviderId: '5836d7cff2e48f3103850699',
+      PatientId: '5836db02f2e48f36ba90ac4a'
+    }
+    this.patientService.AppointmentsByPatientId(reqparams).subscribe((resp) => {
       this.appointments = resp.ListResult;
     });
   }
