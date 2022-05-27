3d453ffe00cd65e41324e30299e60fadf1317f17
shweta-ops <shweta@calibrage.in>
2022-05-27 18:54:16 +0530

    Integrated UI Development In My profile And New Message screen.

---
+++ src/app/_models/_patient/patientprofile.ts
@@ -0,0 +1,30 @@
+export interface PatientProfile {
+  PatientId: string;
+  ProfileImage: string;
+  FirstName: string;
+  MiddleName: string;
+  LastName: string;
+  DateOfBirth: string;
+  Sex: string;
+  HomePhone: string;
+  CellPhone: string;
+  WorkPhone:string;
+  UserId:string;
+  email: string;
+  StreetAddress: string;
+  city:string;
+  state:string;
+  zip:string;
+  EmergencyId:string
+  EmergencyFirstName:string;
+  EmergencyMiddleName:string
+  EmergencyLastName:string
+  RelationshipToPatient:string
+  Phone:string
+  EmergencyEmail:string
+  EmergencyStreetAddress:string
+  EmergencyCity:string
+  EmergencyState:string
+  EmergencyZip:string
+}
+
