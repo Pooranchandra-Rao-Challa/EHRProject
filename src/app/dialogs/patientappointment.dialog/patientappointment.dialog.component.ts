c77dd9c5fe87744f37c0d67cefd469f80ce1f101
Anusha Vutukuri <anusha@calibrage.in>
2022-07-13 18:23:14 +0530

    dropdown issues resolved and alignment issues resolved

--- src/app/dialogs/patientappointment.dialog/patientappointment.dialog.component.ts
+++ src/app/dialogs/patientappointment.dialog/patientappointment.dialog.component.ts
@@ -1,20 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { EHROverlayRef } from 'src/app/ehr-overlay-ref';
-
+import { AuthenticationService } from 'src/app/_services/authentication.service';
+import { User } from '../../_models';
+import { PatientService } from '../../_services/patient.service'
 @Component({
   selector: 'app-patientappointment.dialog',
   templateUrl: './patientappointment.dialog.component.html',
   styleUrls: ['./patientappointment.dialog.component.scss']
 })
 export class PatientappointmentDialogComponent implements OnInit {
+  user: User;
   displayReq = "none";
   hoverDATE:string='Date';
   hoverTime:string='Date';
-  constructor(private ref: EHROverlayRef) { }
+  Providerdata: any;
+  clinicaldata: any;
+  constructor(private ref: EHROverlayRef,private patientservise: PatientService,private authenticationService: AuthenticationService) {
+    this.user = authenticationService.userValue
+   }
 
   ngOnInit(): void {
+    this.getProviders();
+    this.getLocations();
   }
   cancel() {
     this.ref.close(null);
+  }
+
+  getLocations() {
+    var req={
+      "ClinicId": this.user.ClinicId,
+    }
+    this.patientservise.PatientLocations(req).subscribe(req => {
+        this.clinicaldata = req.ListResult;
+    })
+  }
+  getProviders() {
+    var req={
+      "ClinicId": this.user.ClinicId,
+    }
+    this.patientservise.PatientProviders(req).subscribe(req => {
+        this.Providerdata = req.ListResult;
+    })
   }
 }
