a3f8901a9d3c9c24598787098ee01dc610f9357d
saikarthik1006 <saikarthik@calibrage.in>
2022-07-20 19:47:35 +0530

    updated discontinue popup.

--- src/app/dialogs/discontinue.dialog/discontinue.dialog.component.ts
+++ src/app/dialogs/discontinue.dialog/discontinue.dialog.component.ts
@@ -1,5 +1,7 @@
+import { filter } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 import { EHROverlayRef } from '../../ehr-overlay-ref';
+import { Medication, GlobalConstants, PatientChart } from 'src/app/_models';
 
 @Component({
   selector: 'app-discontinue.dialog',
@@ -7,13 +9,30 @@ import { EHROverlayRef } from '../../ehr-overlay-ref';
   styleUrls: ['./discontinue.dialog.component.scss']
 })
 export class DiscontinueDialogComponent implements OnInit {
+  patientMedication: Medication = new Medication();
+  reasonCodes: GlobalConstants;
+  reasonCodesFilter: GlobalConstants;
 
-  constructor(private ref: EHROverlayRef) { }
+  constructor(private ref: EHROverlayRef) {
+    this.patientMedication = new Medication;
+  }
 
   ngOnInit(): void {
+    this.reasonCodes = GlobalConstants.PROCEDURE_REASON_CODES;
+    this.reasonCodesFilter = this.reasonCodes.slice();
   }
 
   cancel() {
     this.ref.close(null);
   }
+
+  selectReason(reason: any) {
+    this.patientMedication.ReasonCode = reason.Code;
+    this.patientMedication.ReasonDescription = reason.Description;
+  }
+
+  saveReason(reason) {
+    this.ref.close(reason);
+  }
+
 }
