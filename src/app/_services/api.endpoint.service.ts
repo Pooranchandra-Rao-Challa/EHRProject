f35b0bf58c7f2b574610645768ece956210140d6
saikarthik1006 <saikarthik@calibrage.in>
2022-07-28 19:06:11 +0530

    completed intervention services

--- src/app/_services/api.endpoint.service.ts
+++ src/app/_services/api.endpoint.service.ts
@@ -557,8 +557,20 @@ export class APIEndPoint extends EndpointBase {
     return this._baseUrl + "Vaccines";
   }
 
+  get _searchInterventionCodesUrl() {
+    return this._baseUrl + "SearchInterventionCodes";
+  }
+
   get _allergyNamesUrl() {
     return this._baseUrl + "AllergyNames";
+  }
+
+  get _createInterventionsUrl() {
+    return this._baseUrl + "CreateInterventions";
+  }
+
+  get _interventionsByPatientIdUrl() {
+    return this._baseUrl + "InterventionsByPatientId";
   }
 
   get _changePasswordsUrl() {
