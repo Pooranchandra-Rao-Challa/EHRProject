c395988f03f2a15671c16608902d814b672ba9d9
RiyaGupta10 <riya@cis10137>
2022-11-28 10:22:15 +0530

    completed logic for patient relations portal.

--- src/app/account/firsttimepatientlogin/patient.relations.component.ts
+++ src/app/account/firsttimepatientlogin/patient.relations.component.ts
@@ -1,4 +1,8 @@
+import { Router } from '@angular/router';
+import { PatientService } from 'src/app/_services/patient.service';
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/_services/authentication.service';
+import { PatientRelationInfo } from 'src/app/_models/_provider/patientRelationship';
 
 @Component({
   selector: 'app-patient-relations',
@@ -6,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient.relations.component.scss']
 })
 export class PatientRelationsComponent implements OnInit {
+  patientRelationShipInfo: PatientRelationInfo[];
 
-  constructor() {}
+  constructor(private authenticationService: AuthenticationService,
+    private patientService: PatientService,
+    private router: Router) {}
 
   ngOnInit() {
+    this.getPatientRelations();
+  }
+
+  defaultSelectedValue: string;
+
+  getPatientRelations() {
+    let reqParams = {
+      'PatientId': this.authenticationService.userValue.PatientId
+    }
+    this.patientService.PatientRelationInfo(reqParams).subscribe((resp) => {
+      if(resp.IsSuccess) {
+        this.patientRelationShipInfo = resp.Result;
+        this.defaultSelectedValue = this.patientRelationShipInfo[0].PatientId;
+      }
+      else this.patientRelationShipInfo = [];
+    });
+  }
+
+  showNextBtn: boolean = false;
+
+  patientRelationInfo(selectedRelation: PatientRelationInfo){
+    this.authenticationService.UpdatePatientUser(selectedRelation);
+    this.showNextBtn = true;
+    // this.router.navigate(['patient/dashboard']);
   }
 
 }
