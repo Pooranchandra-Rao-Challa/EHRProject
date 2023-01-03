03f98cc187b74f62139daf5d90f7848f8e65f8b0
pcraochallas@calibrage.in <pcraochallas@calibrage.in>
2023-01-03 11:42:57 +0530

    environment files update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   baseUrl: 'http://localhost:4345/',
+  //baseUrl: 'http://172.31.26.238:126/',
   //baseUrl: 'http://182.18.157.215/EHR/API/',
   RX_END_POINT: 'https://rxnav.nlm.nih.gov/',
   RX_URI_NDC_PROPERTIES:'REST/rxcui/{0}/properties.json',
