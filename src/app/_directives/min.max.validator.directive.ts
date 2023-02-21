bebb9e540bbfef8d54881071a190a73c4ff1ad87
pcraochallas@calibrage.in <pcraochallas@calibrage.in>
2023-02-21 12:00:09 +0530

    Min Max Directive

---
+++ src/app/_directives/min.max.validator.directive.ts
@@ -0,0 +1,34 @@
+import { Directive, Input } from '@angular/core';
+import { NG_VALIDATORS, Validator, FormControl } from '@angular/forms';
+
+@Directive({
+  selector: '[minvalue][formControlName],[minvalue][formControl],[minvalue][ngModel]',
+  providers: [{provide: NG_VALIDATORS, useExisting: MinValueDirective, multi: true}]
+})
+export class MinValueDirective implements Validator {
+  @Input()
+  minvalue: number;
+
+  validate(c: FormControl): {[key: string]: any} {
+
+      let v = c.value;
+      console.log(v);
+
+      return ( v < this.minvalue)? {"minvalue": true} : null;
+  }
+}
+
+
+@Directive({
+  selector: '[maxvalue][formControlName],[maxvalue][formControl],[maxvalue][ngModel]',
+  providers: [{provide: NG_VALIDATORS, useExisting: MaxValueDirective, multi: true}]
+})
+export class MaxValueDirective implements Validator {
+  @Input()
+  maxvalue: number;
+
+  validate(c: FormControl): {[key: string]: any} {
+      let v = c.value;
+      return ( v > this.maxvalue)? {"maxvalue": true} : null;
+  }
+}
