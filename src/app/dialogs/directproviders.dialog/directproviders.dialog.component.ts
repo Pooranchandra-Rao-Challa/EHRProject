0f2f33b04886dc946ce25b4fa51292e3c79f7326
SaiKarthik1006 <riya@cis10137>
2023-05-24 18:36:26 +0530

    completed design for direct providers dialog.

---
+++ src/app/dialogs/directproviders.dialog/directproviders.dialog.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { EHROverlayRef } from 'src/app/ehr-overlay-ref';
+import { OverlayService } from 'src/app/overlay.service';
+import { Actions } from 'src/app/_models';
+import { DirectmailDialogComponent } from '../directmail.dialog/directmail.dialog.component';
+
+@Component({
+  selector: 'app-directproviders.dialog',
+  templateUrl: './directproviders.dialog.component.html',
+  styleUrls: ['./directproviders.dialog.component.scss']
+})
+export class DirectprovidersDialogComponent implements OnInit {
+  title: string = "Direct Providers";
+  showDirectProviders: boolean = false;
+  ActionTypes = Actions;
+  directmailDialogComponent = DirectmailDialogComponent;
+
+  constructor(private ref: EHROverlayRef,
+    public overlayService: OverlayService,) { }
+
+  ngOnInit(): void {
+  }
+
+  onDirectProviders() {
+    this.title = "New Message (Direct)";
+    this.showDirectProviders = true;
+  }
+
+  cancel() {
+    this.ref.close(null);
+  }
+
+  onUse(mail) {
+    this.ref.close(mail);
+  }
+
+}
